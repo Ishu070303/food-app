d697e4bfd7dd2b52b9f4e9ad42fc4ad101b26dea
Eshika rawat <eshikarawat02@gmail.com>
2023-10-22 14:45:16 +0530

    added animations and input field

--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 import { LoginInput } from '../components';
 import { LoginBg, Logo } from '../assets';
+import { FaEnvelope, FaLock } from '../assets/icons';
 
 const Login = () => {
   const [ userEmail, setUserEmail ] = useState("");
   const [ isSignUp, setIsSignUp ] = useState(false);
+  const [ password, setPassword ] = useState("");
+  const [ confirm_password, setConfirm_password ] = useState("");
 
 
   return (
@@ -30,10 +34,45 @@ const Login = () => {
 
             {/* input section */}
             <div className='w-full flex flex-col items-center justify-center gap-6 px-4 md:px-12 py-4'>
-              <LoginInput placeholder={"Email Here"} icon={userEmail} inputStateFunc={setUserEmail} type="email" isSignUp={isSignUp} />
+              <LoginInput 
+              placeholder={"Email Here"} 
+              icon={<FaEnvelope className='text-xl text-textColor' />} 
+              inputState={userEmail}
+              inputStateFunc={setUserEmail} 
+              type="email" 
+              isSignUp={isSignUp} 
+              />
+
+              <LoginInput 
+              placeholder={"Password Here"} 
+              icon={<FaLock className='text-xl text-textColor' />} 
+              inputState={password}
+              inputStateFunc={setPassword} 
+              type="password" 
+              isSignUp={isSignUp} 
+              />
+
+            { isSignUp && (  <LoginInput 
+              placeholder={"Confirm Password Here"} 
+              icon={<FaLock className='text-xl text-textColor' />} 
+              inputState={confirm_password}
+              inputStateFunc={setConfirm_password} 
+              type="password" 
+              isSignUp={isSignUp} 
+            />)}
+
+            {!isSignUp ? (
+            <p>
+              Doesn't have an account? {" "}
+              <motion.button whileTap={{ scale: 0.95 }}>
+                Create One
+              </motion.button> 
+            </p> 
+            ): (
+            <p></p>
+            )}
             </div>
         </div>
-
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import App from './App';
 import "./index.css";
 
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 const root = reactDom.createRoot(document.getElementById("root"));
 
 root.render(
     <React.StrictMode>
         <Router>
-            <App />
+            <AnimatePresence>
+                <App />
+            </AnimatePresence>
         </Router>
     </React.StrictMode>
 )
