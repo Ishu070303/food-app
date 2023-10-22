32a7dea773d86b7c58abf3c92b9548b45e3af411
Eshika rawat <eshikarawat02@gmail.com>
2023-10-22 12:11:26 +0530

    added images

---
+++ client/src/assets/index.js
@@ -0,0 +1,1 @@
+export { default as LoginBg } from './img/backgroundImage.png';
--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { LoginBg } from '../assets';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen relative overflow-hidden flex'>
+        {/* background color */}
+        <img 
+        src={LoginBg} className='w-full h-full object-cover absolute top-0 left-0' 
+        alt='' 
+        />
+
+        {/* content box */}
+        <div className='flex flex-col items-center bg-lightOverlay w-[80%] md:w-508 h-full z-10 backdrop-blur-md absolute right-0'>
+
+        </div>
+
+    </div>
   )
 }
 
-export default Login
+export default Login 
