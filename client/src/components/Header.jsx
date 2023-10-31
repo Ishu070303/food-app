b8c948d8873e3c964f8c366eb47bb6beafbd8db5
Eshika rawat <eshikarawat02@gmail.com>
2023-10-31 16:11:06 +0530

    added navbar in main

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Logo } from '../assets'
+
+const Header = () => {
+  return (
+    <header className='fixed backdrop-blur-md z-50 inset-x-0 top-0 flex items-center justify-between px-12 md:px-20 py-6'>
+        <NavLink to={"/"} className="flex items-center justify-center gap-4">
+            <img src={Logo} className='w-12' alt='' />
+            <p className='font-semibold text-xl'>MASTAYYYY</p>
+        </NavLink>
+
+        <nav className='flex items-center justify-center gap-8'>
+            <ul className='hidden md:flex items-center justify-center gap-16'>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/menu"}>Menu</NavLink>
+                <NavLink to={"/services"}>Services</NavLink>
+                <NavLink to={"/aboutus"}>About us</NavLink>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
